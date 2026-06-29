9ca2678ff84bda7fe37a2699c1e9e1fb7d3fce4c
Breninhore <150105050+Breninhore@users.noreply.github.com>
2026-06-28 22:21:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Efeito simples ao rolar página
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        header.style.background = "#000";
+    } else {
+        header.style.background = "#111";
+    }
+});
